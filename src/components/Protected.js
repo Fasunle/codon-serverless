8ff9ac68bcef8650cf1ac1da039611e2250cd31f
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-02-24 08:44:47 +0100

    Unauthenticated users can not see this page
    - users that are not signin or who does not have an account will be redirrected to profile page so as to signup there

---
+++ src/components/Protected.js
@@ -0,0 +1,23 @@
+import { Auth } from "aws-amplify";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Container from "./Container";
+
+const Protected = () => {
+  // helper hook provided by react-router-dom
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().catch(() => {
+      navigation("/profile");
+    });
+  }, []);
+
+  return (
+    <Container>
+      <h2>Hello Protected</h2>
+    </Container>
+  );
+};
+
+export default Protected;
