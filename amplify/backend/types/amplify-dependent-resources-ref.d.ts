9c438e8d0f4b1fc9d05aec37ec595a540e136c5e
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-02-24 08:11:28 +0100

    AWS authentication default setup
    [] signin by username
    [x] unauthenticated users are not able to view the protected route
    [x] the are redirected to the profile page where they can signin or signup
    [x] every use can access the public route

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "codon978a33b5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
